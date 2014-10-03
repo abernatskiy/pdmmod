b6670526d4e8d5c0acc5da6f0bb9eaefd448e2ce
lisa <elizaveta.guseva@stonybrook.edu>
2014-10-02 22:48:49 -0400

    added header for the class Parameter wrote attributes for species
    	new file:   parameter.h
    	modified:   specie.h

---
+++ parameter.h
@@ -0,0 +1,10 @@
+
+
+class Parameter
+{
+public:
+    Parameter();
+    short type;
+    
+    
+};
--- specie.h
+++ specie.h
@@ -1,8 +1,14 @@
 #include <vector>
+#include <map>
+#include parameter.h
+
+//std::map<std::string,Parameter> globParams;
+
 
 class Specie
 {
 public:
     Specie();
-    
+    int length;
+    bool ifCatalyst;
 };

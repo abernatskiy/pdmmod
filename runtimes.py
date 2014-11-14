f71ec3fa0c881decbb34c12b902f8a509cde7b3e
lisa <elizaveta.guseva@stonybrook.edu>
2014-11-13 19:42:03 -0500

    runtimes.py more or less finished
    	modified:   populations.txt
    	modified:   runtimes.py

--- runtimes.py
+++ runtimes.py
@@ -50,7 +50,7 @@ def analyzeRuntime(command,runs,minpop,multiplier,numPoints,plot=True):
         runtimes.append((number*2,pair[0],pair[1]))
         number=number*multiplier
     
-    print('optimization: ',optimization.curve_fit(func, [r[0] for r in runtimes], [r[1] for r in runtimes],[0.0,0.0],[r[2] for r in runtimes]))
+    a, b = optimization.curve_fit(func, [r[0] for r in runtimes], [r[1] for r in runtimes],[0.0,0.0],[r[2] for r in runtimes])[0]
     
     ratios=[]
     for i in range(1,len(runtimes)):
@@ -59,9 +59,8 @@ def analyzeRuntime(command,runs,minpop,multiplier,numPoints,plot=True):
         ratios.append((runtimes[i][0],m,e))
     
     
-    ax0.plot(element[0],func(element[0],0.00057,0.0),'--')
-    fig, (ax0, ax1) = plt.subplots(nrows=2)
-    ax0.plot()
+    
+    fig, (ax0, ax1) = plt.subplots(nrows=2,sharex=True)
     for i in range(len(runtimes)):
         element=runtimes[i]
         ax0.errorbar(element[0],element[1],yerr=element[2],  fmt='-o')
@@ -71,16 +70,20 @@ def analyzeRuntime(command,runs,minpop,multiplier,numPoints,plot=True):
             ax1.errorbar(rat[0],rat[1],yerr=rat[2],fmt='-o')
             #ax1.set_yscale('log')
             #ax1.set_xscale('log')
-        
+    ys=[func(r[0],a,b) for r in runtimes]
+    ax0.plot([r[0] for r in runtimes],ys,'r',label='%.2e'%b+'x'+'%.2e'%a)
+    ax0.legend()
+    ax0.set_title('Simulation run time vs number of species in the simulation')
+    ax1.set_title('Ratio of runtimes as number of species doubles')
     plt.show()
     return runtimes
 
 number=50
 command = './pdmmod', '50', '1', 'x'
-runs = 3
+runs = 5
 minpop=number
 multiplier=2
-numPoints=5
+numPoints=7
 
 rD=analyzeRuntime(command,runs,minpop,multiplier,numPoints)
 

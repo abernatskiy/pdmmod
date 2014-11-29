bb1f6b88a90e1e819261d3cf75e011ff8dec3b59
lisa <elizaveta.guseva@stonybrook.edu>
2014-11-28 20:21:46 -0500

    models which I forgot to add before 	new file:   model006.cpp 	new file:   model006.h 	new file:   model007.cpp 	new file:   model007.h

---
+++ model006.cpp
@@ -0,0 +1,65 @@
+#include <map>
+#include <string>
+#include <iostream>
+#include "specie.h"
+
+/* rigid growing balls
+ * binary polymers
+ * sequences grow on its own and never decay
+ */
+
+std::map<std::string,Parameter> globParams;
+
+Specie::Specie(std::string id){
+    m_id = id;
+    m_length = m_id.length();
+}
+//Overloading <<
+std::ostream& operator<<(std::ostream& os, const Specie& sp)
+{
+    os << sp.m_id << " (of length " << sp.m_length << ")";
+    return os;
+}
+//methods
+
+
+//Defining reactions here
+
+std::list<Reaction> Specie::reactions(Specie specie){
+    //all the reactions two species can have
+    std::list<Reaction> allReactions;
+
+    //nothing is being produced from vacuum in this model
+    if (m_id==std::string("")){}
+    else if(m_id==specie.m_id){
+        for (int i=1; i<m_id.length();i++){
+            Reaction hidr(m_id, 1, specie.m_id, 0, HIDR_RATE);
+            hidr.addProduct(m_id.substr(0,i),1);
+            hidr.addProduct(m_id.substr(i,m_id.length()-i),1);
+            allReactions.push_back(hidr);
+        }
+            if (m_id.find(std::string("1"))==std::string::npos){
+                Reaction growth0(m_id, 1, specie.m_id, 1, FAST_RATE);
+                growth0.addProduct(m_id+std::string("0"),2);
+                allReactions.push_back(growth0);
+                Reaction growth1(m_id, 1, specie.m_id, 0, GROWTH_RATE);
+                growth0.addProduct(m_id+std::string("1"),1);
+                allReactions.push_back(growth0);
+            }
+            else{
+                Reaction growth0(m_id, 1, specie.m_id, 0, GROWTH_RATE);
+                growth0.addProduct(m_id+std::string("0"),1);
+                allReactions.push_back(growth0);
+                Reaction growth1(m_id, 1, specie.m_id, 0, GROWTH_RATE);
+                growth0.addProduct(m_id+std::string("1"),1);
+                allReactions.push_back(growth0);
+            }
+        }
+    else{}
+    
+    return allReactions;
+}
+
+Specie::~Specie(){
+
+}
---
+++ model006.h
@@ -0,0 +1,40 @@
+#ifndef __SPECIE_H
+#define __SPECIE_H
+
+#include <vector>
+#include <string>
+#include <list>
+#include "parameter.h"
+#include "reaction.h"
+
+/*binary polymers
+ * sequences grow on its own and never decay
+ */
+
+
+const float GROWTH_RATE = 0.01;
+const float FAST_RATE = 0.01;
+const float HIDR_RATE = 0.001;
+
+
+class Specie
+{
+public:
+    Specie(){};
+    Specie(std::string id);
+    ~Specie();
+    
+    //attributes
+    std::string m_id;
+    int m_length;
+    
+    //constants
+    
+    
+    //methods
+    std::list<Reaction> reactions(Specie specie);
+    
+    friend std::ostream& operator<<(std::ostream& os, const Specie& sp);
+};
+
+#endif // __SPECIE_H

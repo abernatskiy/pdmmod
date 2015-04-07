92a2e1db68374e8ac8dca70316aaaeeab82d06a7
lisa <elizaveta.guseva@stonybrook.edu>
2015-04-07 14:32:48 -0400

    I wrote a peculiar way of parallization and sync. Sync is especially impressive...
    It requires test in the cluster, which requires gcc
    	modified:   libSimulate.py
    	deleted:    models/012/012_output0/means.txt
    	deleted:    models/012/012_output0/parameters.txt
    	modified:   models/012/012_output0/sim.log
    	deleted:    models/012/012_output0/standDivs.txt
    	deleted:    models/012/012_output0/times.txt
    	deleted:    models/012/012_output1/means.txt
    	deleted:    models/012/012_output1/parameters.txt
    	deleted:    models/012/012_output1/sim.log
    	deleted:    models/012/012_output1/standDivs.txt
    	deleted:    models/012/012_output1/times.txt
    	deleted:    models/012/012_output2/means.txt
    	deleted:    models/012/012_output2/parameters.txt
    	deleted:    models/012/012_output2/sim.log
    	deleted:    models/012/012_output2/standDivs.txt
    	deleted:    models/012/012_output2/times.txt
    	modified:   routes.py

--- routes.py
+++ routes.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python
 from localRt import rt as localRoute
+from localRt import python as pythonBin
 route=localRoute
 routePDM=localRoute+'pdmmod/'
 routeHP=routePDM+'hp-model-scripts/'
 routeResults=routePDM+'sim-results/'
+path2python = pythonBin

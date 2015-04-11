423542a8c29df07c53ee32da3438da43841c8195
Anton Bernatskiy (yaeyama) <abernats@uvm.edu>
2015-04-11 22:13:39 +0000

    New output system based on DataLogger class, untested
    	renamed:    libpdmmod/output.h -> libpdmmod/dataLogger.h
    	deleted:    libpdmmod/output.cpp
    	new file:   libpdmmod/dataLogger.cpp
    	new file:   tests/float/floatDivisibility.c

--- libpdmmod/dataLogger.h
+++ libpdmmod/dataLogger.h
@@ -1,21 +1,19 @@
-#ifndef __OUTPUT_H
-#define __OUTPUT_H
+#ifndef __DATALOGGER_H
+#define __DATALOGGER_H
 
-#include <iostream>
 #include <fstream>
-#include <sstream>
 #include <string>
-#include <map>
+
 #include "totalPopulation.h"
 
 /*
- *  //DATA
+ *  DATA
     Time        // total time of simulatios
     Steps       //how many time points we want to keep
     currentTime //time which is being stored
     currentPop  //population at currentTime calculate via calcPopAtTime
 
-    //METHODS
+    METHODS
     storePopulations(tp)
         //stores couple of (name, population)
     storeTime(tp)
@@ -38,17 +36,34 @@
         // of the simulation (0 if indefinite), tau is the simulation time between records,
         // filename is the name of the original output file. All times are in seconds.
  *
-*/
+ */
 
-std::string storePopulations(TotalPopulation* tp);
-    //stores a list of entities of class Population
+class DataLogger
+{
+public:
+    Logger(TotalPopulation* tp, float timeStep, float totalTime, std::string fileName); // will cause the program to fail if called with bad arguments
+    Logger(TotalPopulation* tp, int recordingPeriod, int totalReactions, std::string fileName); // will cause the program to fail if called with bad arguments
+    ~Logger();
+    void makeHeader(int argc, char** argv);
+    bool makeRecords();
+    void makePostsimulationRecords();
 
-void writeHeaderToFile(TotalPopulation* tp, int argc, char** argv, std::ofstream* myfile);
-void writeToFile(std::string populations, float time, std::ofstream* myfile);
-std::string convToString(TotalPopulation* tp);
-std::string writeOrNotTo(float stepLen, TotalPopulation* tp, float prevStep, std::string prevPops, std::ofstream* myfile);
-int closeFile(std::string filename);
-int openFile(std::string filename);
-float getPrevStep(float stepLen, float prevStep, float currTime);
+private:
+    TotalPopulation* m_tp;
+    int m_type;
+    float m_timeStep;
+    float m_totalTime;
+    int m_recPeriod;
+    int m_numSteps;
+    int m_i;
+    float m_prevTime;
+    std::string m_prevPop;
+    std::ofstream m_file;
 
-#endif // __OUTPUT_H
+    bool checkTimes(float timeStep, float totalTime); // makes the app exit with error as soon as totalTime is not float-divisible by timeStep
+    bool checkReacNum(int recordingPeriod, int totalReactions); // makes the app exit with error as soon as totalReactions is not divisible by recordingPeriod
+    void memorizePopulations(); // stores a list of entities of class Population from the saved pointer to TotalPopulation into the internal representation string
+    void makeRecord(float time);
+};
+
+#endif // __DATALOGGER_H

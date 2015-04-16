a787a986eaa6744e324cea112ab0148ea0e69da2
lisa <elizaveta.guseva@stonybrook.edu>
2015-04-15 20:21:01 -0400

    new file:   models/012/paramSet.txt 	new file:   models/012/parameters.py

---
+++ models/012/parameters.py
@@ -0,0 +1,11 @@
+correspond = {
+            0:('monomerBirthH','float'),
+            1:('monomerBirthP','float'),
+            2:('growth','float'),
+            3:('unfoldedDegradation','float'),
+            4:('unfolding','float'),
+            5:('foldedDegradation','float'),
+            6:('hydrophobicEnergy','float'),
+            7:('maxLength','int'),
+            8:('hydrolysisRate','float'),
+            9:('aggregation','float')}

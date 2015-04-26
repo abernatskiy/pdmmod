906b44b0bdbaa92d3ec368153b31f867fa9b3a39
gelisa <elizaveta.guseva@stonybrook.edu>
2015-04-25 22:51:17 -0400

    files which change often removed from repo
    	modified:   clustThem.py
    	modified:   compareThem.py
    	deleted:    models/012/simulateIt.py
    	deleted:    models/013/paramSet.txt
    	deleted:    models/013/simulateIt.py

--- compareThem.py
+++ compareThem.py
@@ -7,10 +7,12 @@ import matplotlib.pyplot as plt
 import clusteredHP
 
 
-cr2 = clusteredHP.ClusteredResults(modelNum=12,simNum=10,minLength=4,maxLength=25,nonSteadyPercent=0.5)
-cr0 = clusteredHP.ClusteredResults(modelNum=12,simNum=12,minLength=4,maxLength=25,nonSteadyPercent=0.5)
-pickle.dump(cr2,open('compareTmp/cr2.p','wb'))
-pickle.dump(cr0,open('compareTmp/cr0.p','wb'))
+#cr2 = clusteredHP.ClusteredResults(modelNum=12,simNum=10,minLength=4,maxLength=25,nonSteadyPercent=0.5)
+#cr0 = clusteredHP.ClusteredResults(modelNum=12,simNum=12,minLength=4,maxLength=25,nonSteadyPercent=0.5)
+#pickle.dump(cr2,open('compareTmp/cr2.p','wb'))
+#pickle.dump(cr0,open('compareTmp/cr0.p','wb'))
+cr2=pickle.load( open( "compareTmp/cr2.p", "rb" ) )
+cr0=pickle.load( open( "compareTmp/cr0.p", "rb" ) )
 
 #subprocess.call(('mkdir',cr.outputDir+'figures/'))
 #cr.plotHPstats(saveFig=True)

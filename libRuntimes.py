ebd37477fc44f1b2155eeb0eab1e2d328fcaa964
lisa <elizaveta.guseva@stonybrook.edu>
2015-05-16 11:23:10 -0400

    removing sratch 	renamed:    runtimes.py -> libRuntimes.py

--- libRuntimes.py
+++ libRuntimes.py
@@ -338,27 +338,4 @@ def plotSeveral(filenames):#TODO
     plt.show()
     return None
 
-###Change Number of species###
-population=50
-modelNum = 8
-termCond = ('simulateReactions',5000, 2500)
-numOfRuns =10
-
-collRate = 0.5
-species=[10,50,100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,
-         1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400,2500,2600,
-         2700,2800,2900,3000,3100,3200,3300,3400,3500,3600,3700,3800,3900,
-         4000,4100,4200,4300,4400,4500,4600,4700,4800,4900,5000,5100,5200,
-         5300,5400,5500,5600,5700,5800,5900,6000,6250,6500,6750,7000]
-#
-#runSeveralChangeNumSpec(modelNum,termCond,numOfRuns,population,species)
-#filenames = [
-    #'balls-pdmmod-c4-10rep.txt',
-    #'collPartSpecTypes-stochkit-c0.txt',
-    #'balls-pssa-c1-10rep.txt'
-    #]
-
-filenames = ['balls-del-pdmmod-c3-10rep.txt']
-#filenames = ['balls-del-circ10-pdmmod-c2-10rep.txt']
-plotSeveral(filenames)
 

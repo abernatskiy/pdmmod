e6291d1f35213829225cd20d6b3358f4cc38d758
lisa <elizaveta.guseva@stonybrook.edu>
2015-09-08 18:57:30 -0400

    forgoten files from model 016
    	new file:   Makefile
    	new file:   model.h
    	new file:   nativeListLoader.h

---
+++ models/016/model.h
@@ -0,0 +1,68 @@
+#ifndef __SPECIE_H
+#define __SPECIE_H
+
+#include <vector>
+#include <string>
+#include <list>
+#include "parameter.h"
+#include "reaction.h"
+
+/*binary polymers
+ * sequences grow on its own and never decay
+ */
+
+
+
+
+class Specie
+{
+public:
+    std::string modelName;
+    Specie(){};
+    Specie(std::string id);
+    ~Specie();
+    
+    //attributes
+    std::string m_id;
+    std::string m_catalyst;
+    std::string m_substrate;
+    bool m_product;
+    int m_length;
+    bool m_folded;
+    int m_native;
+    float m_hydrophobicity;
+    bool m_active;
+    
+    //constants
+    
+    
+    //methods
+    std::list<Reaction> reactions(Specie specie);
+    
+    friend std::ostream& operator<<(std::ostream& os, const Specie& sp);
+    
+private:
+    //imports either H* or P* 
+    void importHorP( std::list<Reaction>& allReactions,Specie other,
+                       float impRate,std::string HorP);
+
+    void degradeIt(std::list<Reaction>& allReactions,Specie specie,
+              float degrRate);
+
+    void aggregateIt(std::list<Reaction>& allReactions,Specie specie,
+                     float aggRate,int aggPower);
+    void hydrolyseIt(std::list<Reaction>& allReactions,Specie specie,
+                     float dH);
+    float URate(float eH, float z);
+    void foldIt(std::list<Reaction>& allReactions,Specie specie,
+                      float eH, float z);
+    void unfoldIt(std::list<Reaction>& allReactions,Specie specie,
+                  float eH, float z);
+    void growIt(std::list<Reaction>& allReactions,Specie specie,
+                float alpha, std::string HorP, int maxLength);
+    void catalyzeIt(std::list<Reaction>& allReactions,Specie specie,
+                    float alpha, float eH,int maxLength);
+};
+
+
+#endif // __SPECIE_H

c3e8109c839dce06023e77728377628e7fe32c06
lisa <elizaveta.guseva@stonybrook.edu>
2015-10-07 10:54:28 -0400

    PEP8 mods 	modified:   experiment.py

--- experiment.py
+++ experiment.py
@@ -1,3 +1,9 @@
+
+"""
+This module defines class Experiment to run several pdmMod simulations 
+with different simulation parameters on biocomp cluster
+"""
+
 import routes
 import sys
 import subprocess
@@ -10,43 +16,59 @@ import result
 import time
 
 
+
+
 class Experiment(object):
     '''class Experiment
     during one experiment values of parameters change
     a vector of changes is giving.
     According to it simulations are run.
     Files MUST be located in the directory with the name of experimentName
-     * self.experiment -- string
-     * self.modelNum -- int
-     * self.now -- bool (is experiment new or we are restoring data)
-     * self.termCond -- (str,float,float) -- termination condition
-     * self.numOfRuns -- int
-     * self.traj -- bool
-     * self.
+    Args:
+        experimentName (str): 
+        name of the folder in which experiment parameters are.
+        modelNum (int): the number of the model in the pdmMod directory 'models'
+        new (bool): is experiment new or we are restoring data
+    Attributes:
+        experiment (str): name of the folder in which experiment parameters are.
+        modelNum (int): the number of the model in the pdmMod directory 'models'
+        new (bool): is experiment new or we are restoring data
+        termCond (tuple): (str, float, float) -- termination condition 
+        (how to terminate, when to terminate, how often to record data), 
+        see pdmmod usage
+        numOfRuns (int):
+        traj -- bool
     '''
-    def __init__(self,experimentName,modelNum,new):
+    # pylint: disable=too-many-instance-attributes
+    def __init__(self, experimentName, modelNum, new):
         self.experiment = experimentName
         self.modelNum = modelNum
         self.new = new
         if not new:
             self.restore()#TODO
-            
-    
+
+
     def __str__(self):
         return self.experiment+' for the model #'+str(self.modelNum)
-    
-    def initNew(self,termCond,numOfRuns,traj):#TODO
+
+    def initNew(self, termCond, numOfRuns, traj):#TODO
+        '''initiates new Experiment instance to run in future
+        * termCond -- Tuple ()
+        '''
         self.termCond = termCond
         self.numOfRuns = numOfRuns
         self.traj = traj
         self.makeParams()
-        
-        
+
+
     def readParameters(self):
-        types = {'float':float, 'int':int, 'bool':bool, 'str':str}
+        '''reads variable and static parameters of the simulation from the
+        corresponding files
+        '''
+        #types = {'float':float, 'int':int, 'bool':bool, 'str':str}
         try:
             p = subprocess.check_call(
-                ['mkdir',self.experiment],stderr=subprocess.PIPE
+                ['mkdir', self.experiment], stderr=subprocess.PIPE
                 )
         except:
             pass
@@ -54,20 +76,26 @@ class Experiment(object):
             raise FileNotFoundError(
                 "There is no directory \""+self.experiment+"\".\n "+
                 "You must have this directory. We created it for you.\n "+ 
-                "Put there files constant.ini and variable.ini.\n constant.ini has the formant of parameters.ini and has all the parameters which are kept constant during the experiment.\n variable.ini has variable parameters it's format is the following:\n parameter1_type parameter1_name\n val1 val2 val3 ...\n parameter2_type parameter2_name\n val1 val2 val3 ...\n types are python types: float, int, bool(0,1), str")
-        
+                "Put there files constant.ini and variable.ini.\n constant.ini"+
+                " has the formant of parameters.ini and has all the parameters"+
+                " which are kept constant during the experiment.\n variable.ini"+
+                " has variable parameters it's format is the following:\n"+
+                "parameter1_type parameter1_name\n val1 val2 val3 ...\n"+
+                "parameter2_type parameter2_name\n val1 val2 val3 ...\n"+
+                " types are python types: float, int, bool(0, 1), str")
+
         #read constants
-        with open(os.path.join(self.experiment,'constant.ini'), 'r') as content_file:
+        with open(os.path.join(self.experiment, 'constant.ini'), 'r') as content_file:
             constant = content_file.read()
-        
+
         #read variables
         self.numOfExperiments = 1
         variables = [] #list of lists of values of variables
-        with open(os.path.join(self.experiment,'variable.ini'), 'r') as content_file:
+        with open(os.path.join(self.experiment, 'variable.ini'), 'r') as content_file:
             variableSep = (content_file.read()).split('\n')
-            if not len(variableSep)%2==0:
+            if not len(variableSep)%2 == 0:
                 print(variableSep)
-                if not variableSep[-1]=='':#dealing with extra empty line
+                if not variableSep[-1] == '':#dealing with extra empty line
                     raise ValueError(
                         'Wrong number of lines in \"variable.ini\" file'
                         )
@@ -78,142 +106,214 @@ class Experiment(object):
                 #varValues are lists of strings of format varName = value
                 line1 = variableSep[2*i].split(' ')
                 line2 = variableSep[2*i+1].split(' ')
-                varValues=[line1[1]+' = '+ val for val in line2]
-                self.numOfExperiments=self.numOfExperiments*len(varValues)
+                varValues = [line1[1]+' = '+ val for val in line2]
+                self.numOfExperiments = self.numOfExperiments*len(varValues)
                 variables.append(varValues)
         variables = list(itertools.product(*variables))
-        
+
         return constant, variables
 
     def getSimNums(self):
-        path='./'#BUG potentially: FIX
-        dirs=(next(os.walk(path))[1])
+        '''simulations of the experiment will have some numbers, which 
+        are set during the simulations' initiation. this function determines
+        these numbers
+        Returns:
+            int -- the number of the first simulation to run
+        '''
+        path = './'#BUG potentially: FIX
+        dirs = (next(os.walk(path))[1])
         nums = []
         for directory in dirs:
             if '_output' in directory:
-                tmp = directory.replace(str("%03d" %self.modelNum)+'_output','')
+                tmp = directory.replace(str("%03d" %self.modelNum)+'_output', '')
                 if tmp == '':
                     nums.append(0)
                 else:
                     nums.append(int(tmp))
-        if nums ==[]:
+        if nums == []:
             currentRun = 0
         else:
             currentRun = max(nums)+1
         print('first run #: '+str(currentRun))
         return currentRun
-    
+
     def makeParams(self):
+        '''writes a parameters files for all the simulation.
+        format of the file: parameters<simulation number>.ini
+        '''
         constant, variables = self.readParameters()
         print(variables)
         firstSim = self.getSimNums()
         self.firstSim = firstSim
-        for i in range(firstSim,firstSim+self.numOfExperiments):
-            pFile=open(
-                os.path.join(self.experiment,'parameters'+str(i)+'.ini'),
+        sFile = open(
+            os.path.join(self.experiment, 'simParams.txt'), 
+                'a'
+                )
+        sFile.write('termCond = '+
+                    str(self.termCond[0])+' '+
+                    str(self.termCond[1])+' '+
+                    str(self.termCond[2])+'\n')
+        sFile.write('numOfRuns = '+str(self.numOfRuns)+'\n')
+        sFile.write('traj = '+str(self.traj)+'\n')
+        sFile.close()
+        for i in range(firstSim, firstSim+self.numOfExperiments):
+            pFile = open(
+                os.path.join(self.experiment, 'parameters'+str(i)+'.ini'), 
                 'a'
                 )
             pFile.write(constant)
             for line in variables[i-firstSim]:
                 pFile.write(line+'\n')
         pFile.close()
-            
-    
+
+
     def printExperiment(self):#TODO
+        '''None
+        '''
         return None
-    
+
     def reorderSims(self):#TODO
+        '''None
+        '''
         return None
-    
-    def initAndRun(self,kernels,onNode=0):
-        jobs={}
-        for i in range(self.firstSim,self.firstSim+self.numOfExperiments):
+
+    def initAndRun(self, kernels, onNode=0):
+        '''initiates and submits jobs to the cluster
+        Args:
+            kernels (int): how many kernels to use for 1 simulation
+            onNode (int): if specified, tells on which node to run
+        '''
+        jobs = {}
+        for i in range(self.firstSim, self.firstSim+self.numOfExperiments):
             s = libSimulate.Simulation(
-                self.modelNum,
-                self.termCond,
-                rewrite=False,
-                specialPath=None,
-                numOfRuns=self.numOfRuns,
-                traj=self.traj,log_level='INFO')
-            jobs[i]=[]
-            paramFile=os.path.join(
-                routes.routePDM,
-                'models',
-                str("%03d" %self.modelNum),
-                self.experiment,
+                self.modelNum, 
+                self.termCond, 
+                rewrite=False, 
+                specialPath=None, 
+                numOfRuns=self.numOfRuns, 
+                traj=self.traj, log_level='INFO')
+            jobs[i] = []
+            paramFile = os.path.join(
+                routes.routePDM, 
+                'models', 
+                str("%03d" %self.modelNum), 
+                self.experiment, 
                 'parameters'+str(i)+'.ini'
                 )
-            populFile=os.path.join(
-                routes.routePDM,
-                'models',
-                str("%03d" %self.modelNum),
-                self.experiment,
+            populFile = os.path.join(
+                routes.routePDM, 
+                'models', 
+                str("%03d" %self.modelNum), 
+                self.experiment, 
                 'populations.txt')
             perKernel = int(math.ceil(s.numOfRuns/kernels))
-            lastKernel = s.numOfRuns - perKernel*(kernels-1)
+            #lastKernel = s.numOfRuns - perKernel*(kernels-1)
             for ker in range(kernels-1):
                 trajFirst = ker*perKernel
                 trajLast = int((ker+1)*perKernel - 1)
                 s.log.info('kernel'+str(ker))
                 s.addToQueue(
-                    s.outputDir,ker,trajFirst,trajLast,jobs[i],onNode,
-                    paramFile,populFile)
+                    s.outputDir, ker, trajFirst, trajLast, jobs[i], onNode, 
+                    paramFile, populFile)
                 print(str(i)+' has been submitted')
             if kernels == 1:
                 ker = -1
                 trajLast = -1
             s.log.info('last kernel')
-            s.addToQueue(s.outputDir,ker+1,trajLast+1,self.numOfRuns-1,jobs[i],onNode,paramFile,populFile)
+            s.addToQueue(
+                s.outputDir, ker+1, trajLast+1, self.numOfRuns-1, jobs[i], 
+                onNode, paramFile, populFile
+                )
         print(jobs)
         return jobs
-    
-    def reorganize(self,jobs,kernels,onNode=0):
+
+    def reorganize(self, jobs):
+        '''for all the jobs when they are done permorms simulation.reorganize():
+        writes means, stds and supplementary files
+        '''
         def checkJobsOnClust():
-            p = subprocess.Popen('qstat', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            '''checks which jobs are still running
+            Returns:
+                [int]: job numbers
+            '''
+            p = subprocess.Popen('qstat', 
+                                 stdout=subprocess.PIPE, 
+                                 stderr=subprocess.PIPE)
             out, err = p.communicate()
 
             lines = (out.decode().split('\n'))[2:]
             jobsOnClust = []
             for line in lines:
-                items =line.split(' ')
+                items = line.split(' ')
                 try:
                     jobsOnClust.append(int(items[1]))
                 except:
                     continue
             return jobsOnClust
-        
-        def ifJobsDone(jobsOnClust,jobNum):
+
+        def ifJobsDone(jobsOnClust, jobNum):
+            '''
+            Returns:
+                True if all the jobs done returns, False otherwise 
+            '''
             jobsDoneList = []
             for job in jobs[jobNum]:
                 if job in jobsOnClust:
                     return False
                 else:
                     jobsDoneList.append(job)
-            if set(jobs[jobNum])==set(jobsDoneList):
+            if set(jobs[jobNum]) == set(jobsDoneList):
                 return True
-            
+
         allJobsDone = False
         jobVector = list(jobs.keys())
         doneVector = []
         while not allJobsDone:
-            for i in range(self.firstSim,self.firstSim+self.numOfExperiments):
+            for i in range(self.firstSim, self.firstSim+self.numOfExperiments):
                 time.sleep(10)
                 jobsOnClust = checkJobsOnClust()
-                jobIsDone = ifJobsDone(jobsOnClust,i)
+                jobIsDone = ifJobsDone(jobsOnClust, i)
                 if jobIsDone and (not i in doneVector):
                     print('job '+str(i)+' is done, calculating means')
                     doneVector.append(i)
                     r = result.Result(
-                        self.modelNum,i,True,self.numOfRuns,traj=True
+                        self.modelNum, i, True, self.numOfRuns, traj=True
                         )
                     print('means are calculated. continue')
-            if set(doneVector)==set(jobVector):
+            if set(doneVector) == set(jobVector):
                 allJobsDone = True
                 print('done')
-            
-    
-    def restore(self):#TODO
+
+    def _readSimParam(self):
+        '''reads simParams.txt
+        in order to restore Experiment instance
+        '''
+        #print(self.experiment)
+        with open(os.path.join(self.experiment, 'simParams.txt'), 'r') as content_file:
+            content = content_file.read()
+        contList = [item.split(' = ') for item in content.split('\n')]
+        for item in contList:
+            if item[0] == 'termCond':
+                data = item[1].split(' ')
+                self.termCond = (data[0], float(data[1]), float(data[2]))
+            elif item[0] == 'traj':
+                self.traj = (item[1] == 'True')
+            elif item[0] == 'numOfRuns':
+                self.numOfRuns = int(item[1])
+            else:
+                continue
+
         return None
+
+
+    def restore(self):#TODO
+        '''given the name of experiment and corresponding _output directories
+        restores Experiment instance
+        '''
+        self._readSimParam()
+        finished = self._ifFinished()
+        plots = self._ifPlots()
+        return finished, plots
 
 
 

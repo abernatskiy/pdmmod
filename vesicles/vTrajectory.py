370960ad0fc8e57778dd36169ff01d051b9cd163
gelisa <elizaveta.guseva@stonybrook.edu>
2016-04-24 17:36:35 -0400

    new file:   vesicles/vTrajectory.py

---
+++ vesicles/vTrajectory.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import os
+import random
+import sys
+sys.path.append('../')
+import routes
+
+from vesicle import *
+from trajectory import *
+
+class vTrajectory(Trajectory):
+    '''
+    '''
+    def __init__(self, modelNum, simNum,generation,)

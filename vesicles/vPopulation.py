5e8606ae26b8c7d94281c24fc6c61d08011c20ad
Elizaveta Guseva <elizaveta.guseva@stonybrook.edu>
2016-05-23 16:06:27 -0400

    parallel running of vesicle simulation for multiple lineages
    	modified:   vPopulation.py

--- vesicles/vPopulation.py
+++ vesicles/vPopulation.py
@@ -11,6 +11,7 @@ import os
 import sys
 import pickle
 import matplotlib.pyplot as plt
+import subprocess
 
 sys.path.append('../')
 import routes
@@ -32,13 +33,13 @@ class VPopulation(object):
         """
         ??
         Args:
-            numGen:
-            numInstance:
-            modelNum:
-            matureWeight:
-            termTime:
-            timeStep:
-            path:
+            numGen: number of generations in every lineage
+            numInstance: number of linages in the population
+            modelNum: number of the model used to model vesicles chemistry
+            matureWeight: weight at which vesicles split
+            termTime: time at which simulations stop
+            timeStep: the time step of the simulations
+            path: path to the main folder containig lineage folders (must be full path!!!)
         """
         self.modelNum = modelNum
         self.numGen = numGen
@@ -85,30 +86,97 @@ class VPopulation(object):
 
         return initFiles
 
-    def runSimulations(self, initFiles, keepAll='select'):  # TEST
+    def writePythonFiles(self,initFiles): #TESTED
+        def wrp(infile,string):
+            return infile.write(string+'\n')
+
+        pythonFiles = []
+        for i in range(self.numInstance):
+            lineagePath = os.path.join(self.path, 'l' + str("%04d" % i))
+            pythonFile = os.path.join(lineagePath,'run.py')
+            pf = open(pythonFile,'w')
+            pf.close()
+            pythonFiles.append(pythonFile)
+            with open(os.path.join(lineagePath,'run.py'),'a') as pf:
+                wrp(pf,'import pickle')
+                wrp(pf,'import sys')
+                wrp(pf,'import os')
+                wrp(pf,'sys.path.add(' + str(routes.routeVesicles) + ')')
+                wrp(pf, 'sys.path.add(' + str(routes.routePDM) + ')')
+                wrp(pf, 'import helperFunctions')
+                wrp(pf,'from vesicles import vesicle')
+                wrp(pf,'allVesicles = []')
+                wrp(pf,'for i in range(' + str(self.numInstance) + '):')
+                wrp(pf,'\t'+'seqs = helperFunctions.readPopulations(' +
+                    str(initFiles[i]) +
+                                   ')')
+                wrp(pf,
+                    '\t'+'v=vesicle.Vesicle('+
+                        'generation=0, sequences=seqs, idInGen=0, motherIdInGen=0,' +
+                        'matureWeight=' + str(self.matureWeight) + ', modelNum=' + str(self.modelNum) + ',' +
+                        'path=' + str(lineagePath) +
+                        ')'
+                    )
+                wrp(pf,
+                    '\t'+'allVesicles.append(v.growSelectTime(' +
+                    str(self.termTime) + ', ' +
+                    str(self.timeStep) + ', ' + str(self.numGen) + ')'
+                    )
+                wrp(pf,'pickle.dump(allVesicles, open(os.path.join(' +
+                    str(lineagePath) + ', "allVesicles.p"), "wb"))')
+        return pythonFiles
+
+    def writeShells(self,pythonFiles,onNode=0):#TESTED
         """
 
         Args:
-            initFiles:
-            keepAll: is one of:
-                'random'
-                True
-                'select'
+            pythonFiles:
+            onNode:
 
         Returns:
-            allVesicles -- list of lists of objects type Vesicle
+
         """
-        allVesicles = []
+        shellFiles = []
         for i in range(self.numInstance):
-            seqs = helperFunctions.readPopulations(initFiles[i])
-            v = vesicle.Vesicle(
-                generation=0, sequences=seqs, idInGen=0, motherIdInGen=0,
-                matureWeight=self.matureWeight, modelNum=self.modelNum,
-                path=os.path.join(self.path, 'l' + str("%04d" % i))
-            )
-            allVesicles.append(v.growAndSplit(self.termTime, self.timeStep, self.numGen, keepAll))
-        pickle.dump(allVesicles, open(os.path.join(self.path, 'allVesicles.p'), 'wb'))
-        return allVesicles
+            pythonFile = pythonFiles[i]
+            shell = os.path.join(self.path, 'l' + str("%04d" % i),'shell.sh')
+            shellFiles.append(shell)
+            inFile = open(shell,'w')
+            inFile.close()
+            inFile = open(shell,'a')
+            inFile.write('#!/bin/bash\n')
+            inFile.write('#$ -S /bin/bash\n')
+            inFile.write('#$ -N p' + str(self.modelNum)+'ves' + str(i) + '\n')
+            inFile.write('#$ -cwd\n')
+            if onNode == 0:
+                inFile.write('#$ -q cpu_long\n')
+            else:
+                if not type(onNode)==int:
+                    inFile.write('#$ -l highio=1\n')
+                    inFile.write('#$ -q cpu_long\n')
+                else:
+                    inFile.write('#$ -q cpu_long@node'+str("%03d" %onNode)+'\n')
+            inFile.write('#$ -P kenprj\n')
+            inFile.write('\n')
+            inFile.write('cd '+routes.routeVesicles+'\n')
+            inFile.write(routes.path2python+' '+str(pythonFile)+'\n')
+            inFile.close()
+
+        return shellFiles
+
+    def runSimulations(self, shellFiles):  # TEST
+        jobsRun = []
+        for i in range(self.numInstance):
+            p = subprocess.Popen(('qsub', shellFiles[i]),
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
+            out, err = p.communicate()
+            time.sleep(1)
+            output = out.decode().split(' ')
+            print(output)
+            jobsRun.append(int(output[2]))
+
+        return jobsRun
 
     def producePickles(self, allVesicles):
         """
@@ -192,9 +260,13 @@ class VPopulation(object):
 ############################
 
 if __name__ == "__main__":
-    vp = VPopulation(50, 30, 18, 20000, 5, 0.0001, './multirun1')
-    initFiles = vp.initPopFiles(7)
-    allVesicles = vp.runSimulations(initFiles, 'select')
+    vp = VPopulation(50, 3, 18, 20000, 5, 0.0001, os.path.join(routes.routeVesicles,'multirun1'))
+    # initFiles = vp.initPopFiles(endTime=7)
+    initFiles = ['1','2','3']
+    pfs = vp.writePythonFiles(initFiles)
+    shellFiles = vp.writeShells(pfs,0)
+    # vp.runSimulations(shellFiles)
+    # allVesicles = vp.runSimulations(initFiles, 'select')
     #allVesicles = pickle.load(open(os.path.join(vp.path, 'allVesicles.p'), 'rb'))
     # headTraj = vp.producePickles(allVesicles)
     #headTraj = pickle.load(open(os.path.join(vp.path, 'headVts.p'), 'rb'))

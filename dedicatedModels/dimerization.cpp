feb7880ed10315ef6948d96429f825e4e11850df
Anton Bernatskiy (ishigaki) <abernats@uvm.edu>
2017-03-09 21:41:16 -0500

    Random seed library support added to dimerization.cpp
    	modified:   dedicatedModels/dimerization.cpp

--- dedicatedModels/dimerization.cpp
+++ dedicatedModels/dimerization.cpp
@@ -5,6 +5,10 @@
 #include <iostream>
 #include <random>
 #include <cmath>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
 
 typedef double REAL;
 typedef double REALTIME;
@@ -12,7 +16,7 @@ typedef double REALTIME;
 const REAL rates[] = {10., 0.2}; // rates[0] for \varnothing->A, rates[1] for 2A->\varnothing
 const unsigned initialPopulation = 5;
 const REALTIME terminationTime = 10.;
-const unsigned numWalks = 100000;
+const unsigned numWalks = 10000;
 
 using namespace std;
 
@@ -52,11 +56,32 @@ unsigned performWalk(unsigned randomSeed)
 	return n;
 }
 
+vector<unsigned> readSeeds()
+{
+	ostringstream ss;
+	ss << "seeds" << numWalks;
+
+	ifstream seedFile(ss.str());
+	if(seedFile.is_open())
+	{
+		vector<unsigned> seeds;
+		string seedStr;
+		while(getline(seedFile, seedStr))
+			seeds.push_back(stoul(seedStr));
+		seedFile.close();
+		return seeds;
+	}
+	else
+	{
+		cout << "Cannot read file " << ss.str() << endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
 int main(int argc, char** argv)
 {
-	REAL sum = 0.;
-	for(int i=0; i<numWalks; i++)
-		sum += performWalk(900000+i);
-	cout << sum/REAL(numWalks) << endl;
+	vector<unsigned> seeds = readSeeds();
+	for(auto it=seeds.begin(); it!=seeds.end(); it++)
+		cout << performWalk(*it) << endl;
 	return 0;
 }
